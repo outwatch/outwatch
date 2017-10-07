e4731839c60767553e3f98d3e156070564368569
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-10-07 12:17:16 +0200

    New store implementation

--- src/main/scala/outwatch/util/Store.scala
+++ src/main/scala/outwatch/util/Store.scala
@@ -2,7 +2,8 @@ package outwatch.util
 
 import cats.effect.IO
 import outwatch.Sink
-import outwatch.dom.createHandler
+import outwatch.dom._
+import outwatch.dom.helpers.STRef
 import rxscalajs.Observable
 import rxscalajs.subscription.Subscription
 
@@ -21,4 +22,20 @@ final case class Store[State, Action](initialState: State, reducer: (State, Acti
 object Store {
   implicit def toSink[Action](store: Store[_, Action]): Sink[Action] = store.sink
   implicit def toSource[State](store: Store[State, _]): Observable[State] = store.source
+
+  private val storeRef = STRef.empty
+
+  def renderWithStore[S, A](initialState: S, reducer: (S, A) => S, selector: String, root: VNode): IO[Unit] = for {
+    store <- IO(Store(initialState, reducer))
+    _ <- storeRef.asInstanceOf[STRef[Store[S, A]]].put(store)
+    _ <- OutWatch.render(selector, root)
+  } yield ()
+
+  def getStore[S, A]: VNodeIO[Store[S, A]] = Pure(
+    storeRef.asInstanceOf[STRef[Store[S, A]]].getOrThrow(NoStoreException)
+  )
+
+  private object NoStoreException extends
+    Exception("Application was rendered without specifying a Store, please use Outwatch.renderWithStore instead")
+
 }
