7678e9cfa07910c0811a465e6aa514f7537e3399
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-10-13 21:57:59 +0200

    Update HTTP usage

--- src/main/scala/outwatch/http/Http.scala
+++ src/main/scala/outwatch/http/Http.scala
@@ -1,5 +1,7 @@
 package outwatch.http
 
+import cats.effect.IO
+import outwatch.dom.VDomHttp
 import rxscalajs.Observable
 import rxscalajs.dom.{Request, Response}
 
@@ -12,27 +14,37 @@ object Http {
   case object Options extends HttpRequestType
   case object Head extends HttpRequestType
 
-  private def request(observable: Observable[Request], requestType: HttpRequestType): Observable[Response] = {
-    observable.switchMap(data => Observable.ajax(data.copy(method = requestType.toString))).share
+  def single(request: Request, method: HttpRequestType): IO[Response] = IO.async { cb =>
+    Observable.ajax(request.copy(method = method.toString))
+      .subscribe(response => cb(Right(response)), err => cb(Left(new Exception(err.toString))))
   }
 
-  private def requestWithUrl(urls: Observable[String], requestType: HttpRequestType) = {
+  def singleWith[A](a: A, method: HttpRequestType)(f: Observable[A] => Observable[Request]): IO[Response] =
+    IO.async { cb =>
+      f(Observable.just(a))
+        .flatMap(data => Observable.ajax(data.copy(method = method.toString)))
+        .subscribe(response => cb(Right(response)), err => cb(Left(new Exception(err.toString))))
+    }
+
+  private def request(observable: Observable[Request], requestType: HttpRequestType): VDomHttp =
+    VDomHttp(IO(observable.switchMap(data => Observable.ajax(data.copy(method = requestType.toString))).share))
+
+  private def requestWithUrl(urls: Observable[String], requestType: HttpRequestType): VDomHttp =
     request(urls.map(url => Request(url)), requestType: HttpRequestType)
-  }
 
-  def get(urls: Observable[String]) = requestWithUrl(urls, Get)
+  def getWithUrl(urls: Observable[String]): VDomHttp = requestWithUrl(urls, Get)
 
-  def getWithBody(requests: Observable[Request]) = request(requests, Get)
+  def get(requests: Observable[Request]): VDomHttp = request(requests, Get)
 
-  def post(requests: Observable[Request]) = request(requests, Post)
+  def post(requests: Observable[Request]): VDomHttp = request(requests, Post)
 
-  def delete(requests: Observable[Request]) = request(requests, Delete)
+  def delete(requests: Observable[Request]): VDomHttp = request(requests, Delete)
 
-  def put(requests: Observable[Request]) = request(requests, Put)
+  def put(requests: Observable[Request]): VDomHttp = request(requests, Put)
 
-  def options(requests: Observable[Request]) = request(requests, Options)
+  def options(requests: Observable[Request]): VDomHttp = request(requests, Options)
 
-  def head(requests: Observable[Request]) = request(requests, Head)
+  def head(requests: Observable[Request]): VDomHttp = request(requests, Head)
 
 
 }
