49d359fd15ab4b191d23cba2860c46ad85ff7e47
Marius Muja <marius.muja@bitlit.com>
2017-11-09 08:52:23 -0800

    Efficiency improvement for the simple emitter builder case.

--- src/main/scala/outwatch/dom/helpers/EmitterBuilder.scala
+++ src/main/scala/outwatch/dom/helpers/EmitterBuilder.scala
@@ -6,23 +6,23 @@ import outwatch.Sink
 import outwatch.dom.{DestroyHook, Emitter, Hook, InsertHook, UpdateHook}
 import rxscalajs.Observable
 
-final case class EmitterBuilder[E <: Event, O] private (
+final case class TransformingEmitterBuilder[E <: Event, O] private[helpers] (
   eventType: String,
   transform: Observable[E] => Observable[O]
 ) {
 
-  def apply[T](value: T): EmitterBuilder[E, T] = map(_ => value)
+  def apply[T](value: T): TransformingEmitterBuilder[E, T] = map(_ => value)
 
-  def apply[T](latest: Observable[T]): EmitterBuilder[E, T] = copy(
+  def apply[T](latest: Observable[T]): TransformingEmitterBuilder[E, T] = copy(
     transform = obs => transform(obs).withLatestFromWith(latest)((_, u) => u)
   )
 
   @deprecated("Deprecated, use '.map' instead", "0.11.0")
-  def apply[T](f: O => T): EmitterBuilder[E, T] = map(f)
+  def apply[T](f: O => T): TransformingEmitterBuilder[E, T] = map(f)
 
-  def map[T](f: O => T): EmitterBuilder[E, T] = copy(transform = obs => transform(obs).map(f))
+  def map[T](f: O => T): TransformingEmitterBuilder[E, T] = copy(transform = obs => transform(obs).map(f))
 
-  def filter(predicate: O => Boolean): EmitterBuilder[E, O] = copy(transform = obs => transform(obs).filter(predicate))
+  def filter(predicate: O => Boolean): TransformingEmitterBuilder[E, O] = copy(transform = obs => transform(obs).filter(predicate))
 
   def -->(sink: Sink[_ >: O]): IO[Emitter] = {
     val observer = sink.redirect(transform).observer
@@ -30,8 +30,31 @@ final case class EmitterBuilder[E <: Event, O] private (
   }
 }
 
+
+final class EmitterBuilder[E <: Event] private(val eventType: String) extends AnyVal {
+
+  @inline private def create[O](transform: Observable[E] => Observable[O]) = new TransformingEmitterBuilder[E, O](
+    eventType, transform
+  )
+
+  def apply[T](value: T): TransformingEmitterBuilder[E, T] = map(_ => value)
+
+  def apply[T](latest: Observable[T]): TransformingEmitterBuilder[E, T] = create(_.withLatestFromWith(latest)((_, u) => u))
+
+  @deprecated("Deprecated, use '.map' instead", "0.11.0")
+  def apply[T](f: E => T): TransformingEmitterBuilder[E, T] = map(f)
+
+  def map[T](f: E => T): TransformingEmitterBuilder[E, T] = create(_.map(f))
+
+  def filter(predicate: E => Boolean): TransformingEmitterBuilder[E, E] = create(_.filter(predicate))
+
+  def -->(sink: Sink[_ >: E]): IO[Emitter] = {
+    IO.pure(Emitter(eventType, event => sink.observer.next(event.asInstanceOf[E])))
+  }
+}
+
 object EmitterBuilder {
-  def apply[E <: Event](eventType: String) = new EmitterBuilder[E, E]( eventType, identity )
+  def apply[E <: Event](eventType: String) = new EmitterBuilder[E](eventType)
 }
 
 trait HookBuilder[E, H <: Hook] {
