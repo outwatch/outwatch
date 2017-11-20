4846f0f418b6d29664c184f48f3f3ebf012d36f9
Felix Dietze <felix@beerleader.net>
2017-11-20 16:57:09 +0100

    Some Pipe/Handler renames, reorder PipeOps

--- src/main/scala/outwatch/Pipe.scala
+++ src/main/scala/outwatch/Pipe.scala
@@ -9,19 +9,22 @@ trait PipeOps[-I, +O] { self : Pipe[I, O]  =>
 
   def mapSink[I2](f: I2 => I): Pipe[I2, O] = Pipe(redirectMap(f), self)
 
-  def collectSink[I2](f: PartialFunction[I2, I]): Pipe[I2, O] = Pipe(self.redirect(_.collect(f)), self)
-
   def mapSource[O2](f: O => O2): Pipe[I, O2] = Pipe(self, self.map(f))
 
-  def collectSource[O2](f: PartialFunction[O, O2]): Pipe[I, O2] = Pipe(self, self.collect(f))
-
-  def filterSource(f: O => Boolean): Pipe[I, O] = Pipe(self, self.filter(f))
-
   def mapPipe[I2, O2](f: I2 => I)(g: O => O2): Pipe[I2, O2] = Pipe(self.redirectMap(f), self.map(g))
+
+
+  def collectSink[I2](f: PartialFunction[I2, I]): Pipe[I2, O] = Pipe(self.redirect(_.collect(f)), self)
+
+  def collectSource[O2](f: PartialFunction[O, O2]): Pipe[I, O2] = Pipe(self, self.collect(f))
 
   def collectPipe[I2, O2](f: PartialFunction[I2, I])(g: PartialFunction[O, O2]): Pipe[I2, O2] = Pipe(
     self.redirect(_.collect(f)), self.collect(g)
   )
+
+
+  def filterSource(f: O => Boolean): Pipe[I, O] = Pipe(self, self.filter(f))
+
 
   def transformSink[I2](f: Observable[I2] => Observable[I]): Pipe[I2, O] = Pipe(self.redirect(f), self)
 
@@ -36,8 +39,9 @@ object Pipe {
   private[outwatch] def apply[I, O](sink: Sink[I], source: Observable[O]): Pipe[I, O] =
     new ObservableSink[I, O](sink, source) with PipeOps[I, O]
 
-  implicit class FilterSink[I, +O](handler: Pipe[I, O]) {
-    def filterSink(f: I => Boolean): Pipe[I, O] = Pipe(handler.redirect(_.filter(f)), handler)
+  // This is not in PipeOps, because I is contravariant
+  implicit class FilterSink[I, +O](pipe: Pipe[I, O]) {
+    def filterSink(f: I => Boolean): Pipe[I, O] = Pipe(pipe.redirect(_.filter(f)), pipe)
   }
 
   /**
@@ -47,12 +51,12 @@ object Pipe {
     * @tparam T the type parameter of the elements
     * @return the newly created Pipe.
     */
-  def create[T](seeds: T*): IO[Pipe[T, T]] = create[T].map { handler =>
+  def create[T](seeds: T*): IO[Pipe[T, T]] = create[T].map { pipe =>
     if (seeds.nonEmpty) {
-      handler.transformSource(_.startWithMany(seeds: _*))
+      pipe.transformSource(_.startWithMany(seeds: _*))
     }
     else {
-      handler
+      pipe
     }
   }
 
