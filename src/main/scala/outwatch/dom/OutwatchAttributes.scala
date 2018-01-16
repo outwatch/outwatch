e2c1279b6e27690b619ddc27f8ddadc176be579a
Marius Muja <mariusmuja@gmail.com>
2018-01-16 00:51:38 -0800

    Make hooks and emitterbuilder consistent, closes #152

--- src/main/scala/outwatch/dom/OutwatchAttributes.scala
+++ src/main/scala/outwatch/dom/OutwatchAttributes.scala
@@ -31,20 +31,20 @@ trait OutWatchLifeCycleAttributes {
     * This hook is invoked once the DOM element for a vnode has been inserted into the document
     * and the rest of the patch cycle is done.
     */
-  lazy val onInsert   = InsertHookBuilder
+  lazy val onInsert   = SimpleEmitterBuilder(InsertHook)
 
   /** Lifecycle hook for component prepatch. */
-  lazy val onPrePatch   = PrePatchHookBuilder
+  lazy val onPrePatch   = SimpleEmitterBuilder(PrePatchHook)
 
   /** Lifecycle hook for component updates. */
-  lazy val onUpdate   = UpdateHookBuilder
+  lazy val onUpdate   = SimpleEmitterBuilder(UpdateHook)
 
   /**
     * Lifecycle hook for component postpatch.
     *
     *  This hook is invoked every time a node has been patched against an older instance of itself.
     */
-  lazy val onPostPatch   = PostPatchHookBuilder
+  lazy val onPostPatch   = SimpleEmitterBuilder(PostPatchHook)
 
   /**
     * Lifecycle hook for component destruction.
@@ -52,7 +52,7 @@ trait OutWatchLifeCycleAttributes {
     * This hook is invoked on a virtual node when its DOM element is removed from the DOM
     * or if its parent is being removed from the DOM.
     */
-  lazy val onDestroy  = DestroyHookBuilder
+  lazy val onDestroy  = SimpleEmitterBuilder(DestroyHook)
 }
 
 /** Snabbdom Key Attribute */
--- src/main/scala/outwatch/dom/helpers/TargetOps.scala
+++ src/main/scala/outwatch/dom/helpers/TargetOps.scala
@@ -1,24 +1,25 @@
 package outwatch.dom.helpers
 
 import org.scalajs.dom.{Event, html}
+import outwatch.dom.Emitter
 
 trait TargetOps {
 
-  implicit class TargetAsInput[E <: Event, O <: Event](builder: EmitterBuilder[E, O]) {
+  implicit class TargetAsInput[E <: Event, O <: Event](builder: EmitterBuilder[E, O, Emitter]) {
 
     object target {
-      def value: EmitterBuilder[E, String] = builder.map(_.target.asInstanceOf[html.Input].value)
+      def value: EmitterBuilder[E, String, Emitter] = builder.map(_.target.asInstanceOf[html.Input].value)
 
-      def valueAsNumber: EmitterBuilder[E, Int] = builder.map(_.target.asInstanceOf[html.Input].valueAsNumber)
+      def valueAsNumber: EmitterBuilder[E, Int, Emitter] = builder.map(_.target.asInstanceOf[html.Input].valueAsNumber)
 
-      def checked: EmitterBuilder[E, Boolean] = builder.map(_.target.asInstanceOf[html.Input].checked)
+      def checked: EmitterBuilder[E, Boolean, Emitter] = builder.map(_.target.asInstanceOf[html.Input].checked)
     }
 
-    def value: EmitterBuilder[E, String] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].value)
+    def value: EmitterBuilder[E, String, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].value)
 
-    def valueAsNumber: EmitterBuilder[E, Int] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].valueAsNumber)
+    def valueAsNumber: EmitterBuilder[E, Int, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].valueAsNumber)
 
-    def checked: EmitterBuilder[E, Boolean] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].checked)
+    def checked: EmitterBuilder[E, Boolean, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].checked)
   }
 
 }
