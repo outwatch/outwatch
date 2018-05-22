4b3fd0e38a9ebe98d349ee7e13d8b4e358d26637
johannes karoff <johannes@karoff.net>
2018-05-22 13:35:39 +0200

    add VDomModifier factory for implicit AsVDomModifier

--- src/main/scala/outwatch/dom/package.scala
+++ src/main/scala/outwatch/dom/package.scala
@@ -9,7 +9,9 @@ package object dom extends Implicits with ManagedSubscriptions with SideEffects 
   object VDomModifier {
     val empty: VDomModifier = IO.pure(EmptyModifier)
 
-    def apply(modifiers: VDomModifier*): VDomModifier = modifiers.sequence.map(CompositeModifier)
+    def apply(modifier: VDomModifier, modifier2: VDomModifier, modifiers: VDomModifier*): VDomModifier =
+      (Seq(modifier, modifier2) ++ modifiers).sequence.map(CompositeModifier)
+    def apply[T](t: T)(implicit as: AsVDomModifier[T]): VDomModifier = as.asVDomModifier(t)
   }
 
   type Observable[+A] = monix.reactive.Observable[A]
