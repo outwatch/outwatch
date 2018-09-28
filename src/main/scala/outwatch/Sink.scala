38bff7ebe1b0ab10afd474912069af5d881ec9ec
johannes karoff <johannes@karoff.net>
2018-09-28 18:19:31 +0200

    sink format/refactor

--- src/main/scala/outwatch/Sink.scala
+++ src/main/scala/outwatch/Sink.scala
@@ -8,14 +8,12 @@ import scala.concurrent.Future
 object Sink {
 
   def create[T](next: T => Future[Ack],
-    error: Throwable => Unit = t => throw t,
+    error: Throwable => Unit = _ => (),
     complete: () => Unit = () => ()
-   ): Observer[T] = {
-    new Observer[T] {
-      override def onNext(t: T): Future[Ack] = next(t)
-      override def onError(ex: Throwable): Unit = error(ex)
-      override def onComplete(): Unit = complete()
-    }
+   ): Observer[T] = new Observer[T] {
+    override def onNext(t: T): Future[Ack] = next(t)
+    override def onError(ex: Throwable): Unit = error(ex)
+    override def onComplete(): Unit = complete()
   }
 
   def fromFunction[T](next: T => Unit): Observer[T] = Sink.create[T]{ t => next(t); Ack.Continue }
