37932d4a79457fb84b632548104e24cd88023966
johannes karoff <johannes@karoff.net>
2018-11-01 15:34:31 +0100

    simplify managed subscriptions

--- src/main/scala/outwatch/dom/ManagedSubscriptions.scala
+++ src/main/scala/outwatch/dom/ManagedSubscriptions.scala
@@ -5,24 +5,17 @@ import cats.implicits._
 import monix.execution.{Cancelable, Scheduler}
 import monix.execution.cancelables.CompositeCancelable
 import org.scalajs.dom
-import outwatch.dom.dsl.attributes.lifecycle
 
 trait ManagedSubscriptions {
 
-  def managed(subscription: IO[Cancelable]): VDomModifier = IO {
-    var cancelable: Cancelable = null
-    VDomModifier(
-      dsl.onDomMount foreach { cancelable = subscription.unsafeRunSync() },
-      dsl.onDomUnmount foreach { cancelable.cancel() }
-    )
-  }
-
+  @inline def managed(subscription: () => Cancelable): VDomModifier = managedElement(_ => subscription())
+  @inline def managed(subscription: IO[Cancelable]): VDomModifier = managed(() => subscription.unsafeRunSync())
   def managed(sub1: IO[Cancelable], sub2: IO[Cancelable], subscriptions: IO[Cancelable]*): VDomModifier = {
     val composite = (sub1 :: sub2 :: subscriptions.toList).sequence.map(subs => CompositeCancelable(subs: _*))
     managed(composite)
   }
 
-  def managedAction(action: Scheduler => Cancelable): VDomModifier = SchedulerAction(scheduler => managed(IO(action(scheduler))))
+  def managedAction(action: Scheduler => Cancelable): VDomModifier = SchedulerAction(scheduler => managed(() => action(scheduler)))
 
   object managedElement {
     def apply(subscription: dom.Element => Cancelable): VDomModifier = IO {
