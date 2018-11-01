151e0c8e13c1d4c132dbbf076cb85281a6b1ce29
johannes karoff <johannes@karoff.net>
2018-11-01 16:57:05 +0100

    inline implicits

--- src/main/scala/outwatch/dom/Implicits.scala
+++ src/main/scala/outwatch/dom/Implicits.scala
@@ -9,11 +9,11 @@ import monix.execution.Scheduler
 
 trait Implicits {
 
-  implicit def asVDomModifier[T](value: T)(implicit vm: AsVDomModifier[T]): VDomModifier = vm.asVDomModifier(value)
+  @inline implicit def asVDomModifier[T](value: T)(implicit vm: AsVDomModifier[T]): VDomModifier = vm.asVDomModifier(value)
 
   //TODO: would be better to have typeclass AsObserver on all functions using observer.
   // but if we do this: emitter --> obs will not compile with sideeffects without type parameters
-  implicit def asObserver[T, F[_]](value: F[T])(implicit ao: AsObserver[F]): Observer[T] = ao.as(value)
+  @inline implicit def asObserver[T, F[_]](value: F[T])(implicit ao: AsObserver[F]): Observer[T] = ao.as(value)
 
-  implicit def StyleIsBuilder[T](style: keys.Style[T]): BasicStyleBuilder[T] = new BasicStyleBuilder[T](style.cssName)
+  @inline implicit def StyleIsBuilder[T](style: keys.Style[T]): BasicStyleBuilder[T] = new BasicStyleBuilder[T](style.cssName)
 }
