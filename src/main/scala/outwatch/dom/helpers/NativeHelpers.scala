9e3e78183e63e40c4f3b170966de0040f7df14f8
busti <mbust@live.de>
2018-11-16 12:32:43 +0100

    Provide helper methods for efficiency optimized js.Array append and prepend

--- src/main/scala/outwatch/dom/helpers/NativeHelpers.scala
+++ src/main/scala/outwatch/dom/helpers/NativeHelpers.scala
@@ -3,11 +3,24 @@ package outwatch.dom.helpers
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSBracketAccess
 
+private[outwatch] object JsArrayHelpers {
+  def appendSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
+    case wrappedOther:js.WrappedArray[T] => source.concat(wrappedOther.array)
+    case _                               => source ++ other
+  }
+
+  def prependSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
+    case wrappedOther:js.WrappedArray[T] => wrappedOther.array.concat(source)
+    case _                               => other.++(source)(collection.breakOut)
+  }
+}
+
 @js.native
 private[outwatch] trait DictionaryRawApply[A] extends js.Object {
   @JSBracketAccess
   def apply(key: String): js.UndefOr[A] = js.native
 }
+
 private[outwatch] object NativeHelpers {
   implicit class WithRaw[A](val dict: js.Dictionary[A]) extends AnyVal {
     @inline def raw: DictionaryRawApply[A] = dict.asInstanceOf[DictionaryRawApply[A]]
