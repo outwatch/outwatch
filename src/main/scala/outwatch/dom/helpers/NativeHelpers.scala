c7d0c05d7a14f035650ec47c64179cf32d94a5d6
busti <mbust@live.de>
2018-11-16 12:37:44 +0100

    Move js.Array helper Methods to NativeHelpers

--- src/main/scala/outwatch/dom/helpers/NativeHelpers.scala
+++ src/main/scala/outwatch/dom/helpers/NativeHelpers.scala
@@ -3,18 +3,6 @@ package outwatch.dom.helpers
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSBracketAccess
 
-private[outwatch] object JsArrayHelpers {
-  def appendSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
-    case wrappedOther:js.WrappedArray[T] => source.concat(wrappedOther.array)
-    case _                               => source ++ other
-  }
-
-  def prependSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
-    case wrappedOther:js.WrappedArray[T] => wrappedOther.array.concat(source)
-    case _                               => other.++(source)(collection.breakOut)
-  }
-}
-
 @js.native
 private[outwatch] trait DictionaryRawApply[A] extends js.Object {
   @JSBracketAccess
@@ -27,4 +15,14 @@ private[outwatch] object NativeHelpers {
   }
 
   @inline def assign[T](value: T)(f: T => Unit): T = { f(value); value }
+
+  @inline def appendSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
+    case wrappedOther:js.WrappedArray[T] => source.concat(wrappedOther.array)
+    case _                               => source ++ other
+  }
+
+  @inline def prependSeq[T](source: js.Array[T], other: Seq[T]): js.Array[T] = other match {
+    case wrappedOther:js.WrappedArray[T] => wrappedOther.array.concat(source)
+    case _                               => other.++(source)(collection.breakOut)
+  }
 }
