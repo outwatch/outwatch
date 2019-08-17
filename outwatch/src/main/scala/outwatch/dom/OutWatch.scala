9b3c2d9a10ee803392f6c6382b008f9ecca00c0f
Zak <pattersonzak@gmail.com>
2019-08-17 14:16:17 -0500

    Remove singleton store and unused global STRef class

--- outwatch/src/main/scala/outwatch/dom/OutWatch.scala
+++ outwatch/src/main/scala/outwatch/dom/OutWatch.scala
@@ -5,7 +5,6 @@ import monix.execution.Scheduler
 import org.scalajs.dom
 import org.scalajs.dom._
 import outwatch.dom.helpers.SnabbdomOps
-import outwatch.util.Store
 import snabbdom.{VNodeProxy, patch}
 
 object OutWatch {
@@ -39,22 +38,4 @@ object OutWatch {
 
   @deprecated("Use renderInto instead (or renderReplace)", "0.11.0")
   def render(querySelector: String, vNode: VNode)(implicit s: Scheduler): IO[Unit] = renderInto(querySelector, vNode)
-
-  def renderWithStore[A, M](
-    initialAction: A,
-    initialState: M,
-    reducer: Store.Reducer[A, M],
-    querySelector: String,
-    root: IO[VNode]
-  )(implicit s: Scheduler): IO[Unit] =
-    Store.renderWithStore(initialAction, initialState, reducer, querySelector, root)
-
-  def renderWithStore[A, M](
-    initialAction: A,
-    initialState: M,
-    reducer: Store.Reducer[A, M],
-    querySelector: String,
-    root: VNode
-  )(implicit s: Scheduler): IO[Unit] =
-    Store.renderWithStore(initialAction, initialState, reducer, querySelector, IO.pure(root))
 }
