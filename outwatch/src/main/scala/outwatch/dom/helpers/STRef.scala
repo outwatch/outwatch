fc06528633883d16e754260f3e524e7f99058229
Zak <pattersonzak@gmail.com>
2019-08-20 10:09:13 -0500

    Fix STRef to delay unsafeGet, per review

--- outwatch/src/main/scala/outwatch/dom/helpers/STRef.scala
+++ outwatch/src/main/scala/outwatch/dom/helpers/STRef.scala
@@ -6,9 +6,8 @@ import cats.implicits._
 class STRef[F[_]: Sync, A](private var unsafeGet: A) {
   def put(a: A): F[A] = Sync[F].delay { unsafeGet = a; a }
 
-  def getOrThrow(t: Throwable): F[A] = 
-    unsafeGet.pure[F].flatMap(s => if (s == null) Sync[F].raiseError(t) else s.pure[F]) // scalastyle:ignore
-
+  def getOrThrow(t: Throwable): F[A] = Sync[F].delay(unsafeGet).ensure(t)(_ != null)
+    
   def get: F[A] = getOrThrow(new IllegalStateException())
   def update(f: A => A): F[A] = Sync[F].delay { unsafeGet = f(unsafeGet); unsafeGet }
 }
