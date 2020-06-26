8db18b821e869a280f759a9eeeca16c79d47577d
busti <mbust@live.de>
2020-06-26 13:45:49 +0200

    move global js this helper value to NativeHelpers

--- outwatch/src/main/scala/outwatch/interpreter/SnabbdomOps.scala
+++ outwatch/src/main/scala/outwatch/interpreter/SnabbdomOps.scala
@@ -74,25 +74,13 @@ private[outwatch] object SnabbdomOps {
      }
    }
 
-   // See: https://www.scala-js.org/doc/interoperability/global-scope.html#dynamically-lookup-a-global-variable-given-its-name
-   lazy val globalObject: js.Dynamic = {
-     import js.Dynamic.{global => g}
-     if (js.typeOf(g.global) != "undefined" && (g.global.Object eq g.Object)) {
-       // Node.js environment detected
-       g.global
-     } else {
-       // In all other well-known environment, we can use the global `this`
-       js.special.fileLevelThis.asInstanceOf[js.Dynamic]
-     }
-   }
-
    type SetImmediate = js.Function1[js.Function0[Unit], Int]
    type ClearImmediate = js.Function1[Int, Unit]
    private val (setImmediateRef, clearImmediateRef): (SetImmediate, ClearImmediate) = {
       if (!js.isUndefined(js.Dynamic.global.setImmediate))
-        (js.Dynamic.global.setImmediate.bind(globalObject).asInstanceOf[SetImmediate], js.Dynamic.global.clearImmediate.bind(globalObject).asInstanceOf[ClearImmediate])
+        (js.Dynamic.global.setImmediate.bind(NativeHelpers.globalObject).asInstanceOf[SetImmediate], js.Dynamic.global.clearImmediate.bind(NativeHelpers.globalObject).asInstanceOf[ClearImmediate])
       else
-        (js.Dynamic.global.setTimeout.bind(globalObject).asInstanceOf[SetImmediate], js.Dynamic.global.clearTimeout.bind(globalObject).asInstanceOf[ClearImmediate])
+        (js.Dynamic.global.setTimeout.bind(NativeHelpers.globalObject).asInstanceOf[SetImmediate], js.Dynamic.global.clearTimeout.bind(NativeHelpers.globalObject).asInstanceOf[ClearImmediate])
     }
 
     // we are mutating the initial proxy with VNodeProxy.copyInto, because parents of this node have a reference to this proxy.
