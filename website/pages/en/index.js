59f27d969a853083ae6b22c07368c015ca70a76d
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-12-23 12:12:56 +0100

    Update website texts

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -146,18 +146,18 @@ class Index extends React.Component {
       <Block layout="fourColumn">
         {[
           {
-            content: ['OutWatch combines functional and reactive programming to enable you to build performant web applications using pure functions.',
+            title: 'OutWatch is declarative',
+            content: ['In Outwatch, you can describe your whole web application without doing any side effect - you only run your application when rendering it.',
                       'You won’t see any imperative calls like dispatch or setState.',
                       'Declarative code makes your app easier to reason about and more predictable, allowing you peace of mind.'].join('<br/>'),
-            title: 'OutWatch is declarative'
           },
           {
-            content: 'In OutWatch components are just functions, no boilerplate necessary. Reactive Programming allows us to create fully self-responsible components that never touch external state. No more wondering where an action or a change in state came from. Components are fully decoupled and therefore extremely reusable.',
-            title: 'Simple components'
+            title: 'Simple components',
+            content: 'In OutWatch components are just functions, no boilerplate necessary. Reactive Programming allows you to create fully self-responsible components that never touch external state. No more wondering where an action or a change in state came from. Components are fully decoupled and therefore extremely reusable.',
           },
           {
+            title: 'Complete type safety',
             content: 'With OutWatch typos and type-errors are a thing of the past. Your editor will immediately catch such bugs, without needing to compile. Explore the whole API with its documentation right there in-line with your code.',
-            title: 'Complete type safety'
           }
         ]}
       </Block>
