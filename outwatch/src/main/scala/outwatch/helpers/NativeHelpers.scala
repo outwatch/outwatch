59bef5dc45210d131ec9286a67225303bba5dad3
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-06-01 15:34:39 -0600

    Use @unchecked to avoid asInstanceOf

--- outwatch/src/main/scala/outwatch/helpers/NativeHelpers.scala
+++ outwatch/src/main/scala/outwatch/helpers/NativeHelpers.scala
@@ -31,8 +31,8 @@ private[outwatch] object NativeHelpers {
   @noinline def appendSeq[T](source: js.Array[T], other: collection.Seq[T]): js.Array[T] = if (other.isEmpty) source
   else
     other match {
-      case wrappedOther: js.WrappedArray[_] =>
-        if (source.isEmpty) wrappedOther.asInstanceOf[js.WrappedArray[T]] else source.concat(wrappedOther)
+      case wrappedOther: js.WrappedArray[T @unchecked] =>
+        if (source.isEmpty) wrappedOther else source.concat(wrappedOther)
       case _ =>
         val arr = new js.Array[T]()
         source.foreach(arr.push(_))
@@ -43,9 +43,9 @@ private[outwatch] object NativeHelpers {
   @noinline def prependSeq[T](source: js.Array[T], other: collection.Seq[T]): js.Array[T] = if (other.isEmpty) source
   else
     other match {
-      case wrappedOther: js.WrappedArray[_] =>
-        if (source.isEmpty) wrappedOther.asInstanceOf[js.WrappedArray[T]]
-        else (wrappedOther.asInstanceOf[js.WrappedArray[T]]: js.Array[T]).concat(source)
+      case wrappedOther: js.WrappedArray[T @unchecked] =>
+        if (source.isEmpty) wrappedOther
+        else (wrappedOther: js.Array[T]).concat(source)
       case _ =>
         val arr = new js.Array[T]()
         other.foreach(arr.push(_))
