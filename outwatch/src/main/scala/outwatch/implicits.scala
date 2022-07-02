b76ca86fcb0e9cbc4a825919a296625ea2ccca91
johannes karoff <johannes@karoff.net>
2022-07-02 22:41:58 +0200

    Revert "Revert "add outwatch.implicits for VNode operations on any Functor""
    This reverts commit 50b88ceea4be335524c24f55c0ac33ba324655e8.

---
+++ outwatch/src/main/scala/outwatch/implicits.scala
@@ -0,0 +1,18 @@
+package outwatch
+
+import cats.Functor
+
+object implicits {
+
+  @inline implicit class VNodeFunctorOps[F[_]: Functor](self: F[VNode]) {
+    def apply(args: VModifier*): F[VNode]   = Functor[F].map(self)(_.apply(args: _*))
+    def append(args: VModifier*): F[VNode]  = Functor[F].map(self)(_.append(args: _*))
+    def prepend(args: VModifier*): F[VNode] = Functor[F].map(self)(_.prepend(args: _*))
+  }
+
+  @inline implicit class VNodeFunctor2Ops[F[_]: Functor, G[_]: Functor](self: F[G[VNode]]) {
+    def apply(args: VModifier*): F[G[VNode]]   = Functor[F].map(self)(g => Functor[G].map(g)(_.apply(args: _*)))
+    def append(args: VModifier*): F[G[VNode]]  = Functor[F].map(self)(g => Functor[G].map(g)(_.append(args: _*)))
+    def prepend(args: VModifier*): F[G[VNode]] = Functor[F].map(self)(g => Functor[G].map(g)(_.prepend(args: _*)))
+  }
+}
